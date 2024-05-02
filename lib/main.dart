2c12aa5471e7ca7b515765657f58fb8abdc4ab1f
IdSausage <pongkun.p@gmail.com>
2024-05-02 22:56:42 +0700

    [EDIT] main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/register.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
