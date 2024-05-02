6236029611bf31e89650b015f4498d28fc9aa9f9
IdSausage <pongkun.p@gmail.com>
2024-05-02 23:20:08 +0700

    [EDIT] home_page.dart , main.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'src/widget.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'OKKAMLANGKAI',
-        theme: ThemeData(
-          textTheme: GoogleFonts.robotoTextTheme(),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage());
-  }
-}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
