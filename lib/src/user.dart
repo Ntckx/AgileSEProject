b1060b93b95909c7a9a06d7af59cc5f1cf22885d
IdSausage <pongkun.p@gmail.com>
2024-05-05 17:37:00 +0700

    [ADD] main_page.dart , user.dart
    - add main_page.dart to \lib\auth
    - add user.dart to \lib\src

---
+++ lib/src/user.dart
@@ -0,0 +1,8 @@
+class AppUser {
+  final String username;
+  final String email;
+  final String password;
+
+  const AppUser(
+      {required this.username, required this.email, required this.password});
+}
