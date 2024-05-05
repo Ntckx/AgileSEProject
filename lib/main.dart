cca44bf0f1c57e6625c7793c8257c493a76c965e
IdSausage <pongkun.p@gmail.com>
2024-05-05 17:13:28 +0700

    [EDIT] main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/auth/main_page.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'home_page.dart';
-import 'register.dart';
-import 'registerBodyInformation.dart';
+import 'auth/firebase_options.dart';
+import 'pages/home_page.dart';
+import 'pages/register.dart';
+import 'pages/registerBodyInformation.dart';
 import 'package:go_router/go_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
-final GoRouter _router = GoRouter(routes: [
-  GoRoute(path: '/', builder: (context, state) => Register(), routes: [
-    GoRoute(
-      path: 'body',
-      builder: (context, state) => RegisterBodyInfo(),
-    )
-  ]),
-  GoRoute(
-    path: '/home',
-    builder: (context, state) => const MyHomePage(),
-  )
-]);
+// final GoRouter _router = GoRouter(routes: [
+//   GoRoute(path: '/', builder: (context, state) => Register(), routes: [
+//     GoRoute(
+//       path: 'body',
+//       builder: (context, state) => RegisterBodyInfo(),
+//     )
+//   ]),
+//   GoRoute(
+//     path: '/home',
+//     builder: (context, state) => const MyHomePage(),
+//   )
+// ]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -28,13 +35,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'OKKAMLANGKAI',
       theme: ThemeData(
         textTheme: GoogleFonts.robotoTextTheme(),
         useMaterial3: true,
       ),
-      routerConfig: _router,
+      home: MainPage(),
     );
   }
 }
