d4817342702ddad23f62ec649d63a93dcf60ba2b
IdSausage <pongkun.p@gmail.com>
2024-05-13 02:51:03 +0700

    [EDIT] workout_plan.dart
    - add 4 new member to class which are duration , gestureAmount , descriptions , planId
    - add factory constructor to the class to accomodate the get method in firebase function

--- lib/src/workout_plan.dart
+++ lib/src/workout_plan.dart
@@ -2,7 +2,28 @@ import 'workout.dart';
 
 class WorkoutPlan {
   String name;
+  double duration;
+  int gestureAmount;
   List<Workout> workouts;
+  String description;
+  String planId;
 
-  WorkoutPlan({required this.name, required this.workouts});
+  WorkoutPlan(
+      {required this.name,
+      required this.workouts,
+      required this.duration,
+      required this.gestureAmount,
+      this.description = '',
+      this.planId = ''});
+
+  factory WorkoutPlan.fromFirestore(Map<String, dynamic> map, String planId) {
+    return WorkoutPlan(
+      name: map['planname'] ?? 'no name',
+      duration: map['duration'] ?? 0,
+      gestureAmount: map['gestureAmount'] ?? 0,
+      description: map['description'] ?? '',
+      planId: planId,
+      workouts: [],
+    );
+  }
 }
