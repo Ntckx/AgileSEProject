4518b7f10678191c9fbc7a333432c35fd25358f3
IdSausage <pongkun.p@gmail.com>
2024-05-19 16:11:48 +0700

    [ADD] recommend_workout.dart
    - add list of workout base on user BMI
    which will be later use in generate user plan when registering

---
+++ lib/src/recommend_workout.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_application_1/src/workout.dart';
+
+final List<Workout> workoutListforSkinny = [
+  Workout(workoutName: 'Squat', amount: 10, metValue: 5.0),
+  Workout(workoutName: 'Squat', amount: 10, metValue: 5.0),
+  Workout(workoutName: 'Push up', amount: 20, metValue: 3.8),
+  Workout(workoutName: 'Side arm raise', amount: 30, metValue: 3.0),
+  Workout(workoutName: 'Arm Circles Clockwise', amount: 30, metValue: 3.0),
+  Workout(
+      workoutName: 'Arm Circles Counter Clockwise', amount: 30, metValue: 3.0),
+  Workout(workoutName: 'Leg Raises', amount: 8, metValue: 3.0),
+  Workout(workoutName: 'Leg Raises', amount: 8, metValue: 3.0),
+  Workout(workoutName: 'Russian Twist', amount: 20, metValue: 3.8),
+];
+
+final List<Workout> workoutListForNormal = [
+  Workout(workoutName: 'Lunges', amount: 16, metValue: 4.0),
+  Workout(workoutName: 'Lunges', amount: 16, metValue: 8.0),
+  Workout(workoutName: 'Squat', amount: 24, metValue: 8.0),
+  Workout(workoutName: 'Wall Sit', amount: 30, metValue: 2.3),
+  Workout(workoutName: 'Push up', amount: 20, metValue: 3.8),
+  Workout(workoutName: 'Push-Up & Rotation', amount: 12, metValue: 8.0),
+  Workout(workoutName: 'Burpees', amount: 8, metValue: 8.0),
+  Workout(workoutName: 'Burpees', amount: 8, metValue: 8.0),
+  Workout(workoutName: 'Crossover Crunch', amount: 20, metValue: 3.8),
+  Workout(workoutName: 'Bicycle Crunch', amount: 20, metValue: 3.8),
+  Workout(workoutName: 'Leg Raise', amount: 16, metValue: 3.0),
+];
+
+final List<Workout> workoutListForFat = [
+  Workout(workoutName: 'Side-Lying Leg Lift Left', amount: 10, metValue: 3.0),
+  Workout(workoutName: 'Side-Lying Leg Lift Right', amount: 10, metValue: 3.0),
+  Workout(workoutName: 'Squat', amount: 10, metValue: 8.0),
+  Workout(workoutName: 'Backward Lunge', amount: 10, metValue: 4.0),
+  Workout(workoutName: 'Wall Push up', amount: 20, metValue: 2.3),
+  Workout(workoutName: 'Wall Push up', amount: 30, metValue: 2.3),
+  Workout(workoutName: 'Arm Circles Clockwise', amount: 30, metValue: 3.0),
+  Workout(
+      workoutName: 'Arm Circles Counter Clockwise', amount: 30, metValue: 3.0),
+  Workout(workoutName: 'Russian Twist', amount: 20, metValue: 3.8),
+  Workout(workoutName: 'Heel Touch', amount: 8, metValue: 3.0),
+  Workout(workoutName: 'Mountain Climber', amount: 12, metValue: 8.0),
+];
